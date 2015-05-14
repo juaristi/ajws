feff03f1894fe1983269cc9b9d017485aa5f97dd
Ander Juaristi <ajuaristi@gmx.es>
2015-05-14 15:47:12 +0200

    First commit. Not at all useful, but...

---
+++ main.c
@@ -0,0 +1,39 @@
+/*
+ * main.c
+ *
+ *  Created on: May 14, 2015
+ *      Author: ajuaristi
+ */
+#include <stdio.h>
+#include <pfring.h>
+
+int main(int argc, char **argv)
+{
+	const char *device = "eth0";
+	u_char mac_addr[6];
+
+	pfring *ring = pfring_open(device, 128, 0);
+	if (ring == NULL)
+	{
+		printf("ERROR: Could not open device %s.\n", device);
+		exit(EXIT_FAILURE);
+	}
+
+	if (pfring_get_bound_device_address(ring, mac_addr) == 0)
+	{
+		printf("Using device %s. ", device);
+		printf("MAC address: %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n",
+				mac_addr[0],
+				mac_addr[1],
+				mac_addr[2],
+				mac_addr[3],
+				mac_addr[4],
+				mac_addr[5]);
+	}
+	else
+	{
+		printf("Could not get MAC from device %s.\n", device);
+	}
+
+	pfring_close(ring);
+}
