016f2407be0dd3bc2eabe3569725adc396c3917f
Ander Juaristi <ajuaristi@gmx.es>
2015-06-10 21:44:03 +0200

    Working PF_RING framework

--- main.c
+++ main.c
@@ -6,22 +6,59 @@
  */
 
 #include <stdio.h>
+#include <signal.h>
 #include <pfring.h>
 #include "main.h"
 #include "log.h"
 
+#define APP_NAME "ajws"
+bool interrupted;
+
+void sighandler(int signal)
+{
+	logprintf(LOG_ALWAYS, "Interrupted.\n");
+	interrupted = TRUE;
+}
+
+#ifndef TESTING
 int main(int argc, char **argv)
 {
 	const char *device = "eth0";
 	u_char mac_addr[6];
+#define BUFLEN 512
+	u_char *buf = NULL;
+	u_int bufsiz = 0;
+	struct pfring_pkthdr hdr;
 
 	opt.debug = TRUE;
 	opt.verbose = TRUE;
 
+	/* Register signal handlers */
+	interrupted = FALSE;
+	signal(SIGINT, sighandler);
+
+	/* Allocate buffers */
+	buf = (u_char *) malloc(BUFLEN * sizeof(char));
+	if (!buf)
+	{
+		logprintf(LOG_FATAL, "Could not allocate packet buffer.\n");
+		exit(EXIT_FAILURE);
+	}
+	bufsiz = BUFLEN;
+
+	/* Start PF_RING */
 	pfring *ring = pfring_open(device, 128, 0);
 	if (ring == NULL)
 	{
 		logprintf(LOG_FATAL, "Could not open device %s.\n", device);
+		exit(EXIT_FAILURE);
+	}
+
+	pfring_set_application_name(ring, APP_NAME);
+	pfring_set_direction(ring, rx_and_tx_direction);
+	if (pfring_enable_ring(ring))
+	{
+		logprintf(LOG_FATAL, "The packet ring buffer could not be enabled.\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -41,7 +78,39 @@ int main(int argc, char **argv)
 		logprintf(LOG_FATAL, "Could not get MAC from device %s.\n", device);
 	}
 
+	while (!interrupted)
+	{
+		switch (pfring_recv(ring, &buf, bufsiz, &hdr, 0))
+		{
+		case 0:		/* No packets received. Continue. */
+			break;
+		case 1:		/* We received a packet! */
+			logprintf(LOG_ALWAYS, "Packet received (%d.%ud)\n", hdr.ts.tv_sec, hdr.ts.tv_usec);
+			hexlog(buf, hdr.caplen);
+			break;
+		case -1:	/* Error */
+		default:	/* Fall through. Consider every other value an error. */
+			logprintf(LOG_FATAL, "Could not fetch packet. Exiting.\n");
+			interrupted = TRUE;
+			break;
+		}
+	}
+
 	pfring_close(ring);
+	free(buf);
 
 	return 0;
 }
+#else
+int main(int argc, char **argv)
+{
+	char logstr[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,'A','B','C'};
+
+	opt.debug = TRUE;
+	opt.verbose = TRUE;
+
+	hexlog(logstr, sizeof(logstr));
+
+	return 0;
+}
+#endif
